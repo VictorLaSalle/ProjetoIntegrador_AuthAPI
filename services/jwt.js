f4f42a638232e8ff807066b87d1c01196515c3ce
VictorLaSalle <victor.202031214@unilasalle.edu.br>
2021-10-22 20:33:42 -0300

    fix token header

--- services/jwt.js
+++ services/jwt.js
@@ -1,8 +1,3 @@
 const jwt = require("jsonwebtoken")
 
-module.exports = verifyJWT = (token) => {
-    if(token == null) 
-        return {status: 401, message: "Nenhum token foi provido"}
-
-    return jwt.verify(token, process.env.SECRET, (err, decoded) => err ? err : decoded)
-}
+module.exports = verifyJWT = (token) => jwt.verify(token, process.env.SECRET, (err, decoded) => err ? err : decoded)
